fdc829ea0cd06913718dc6684da58b81adb83b8a
SR <sunilssunilr@gmail.com>
2022-01-01 23:20:45 +0530

    banner completed and content started
    4 banners done with javascript and content started with quotes which is also practicaly done just have to add some effects.tommorow starting with footer then going to about us.

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const rightSlide = document.querySelector("#right-slide");
 const leftSlide = document.querySelector("#left-slide");
-const bannerImg = document.querySelector("#banner-img");
-let translate = 0;
-rightSlide.addEventListener("click",()=>{
-    // alert("right button clicked!")
-    translate = translate + 100;
-    bannerImg.style.transform=`translate(${translate}vw)`;
+const bannerImages = document.querySelectorAll(".image-container");
+
+bannerImages.forEach((image,index)=>
+{
+    image.style.left=`${index*100}%`;
 })
-leftSlide.addEventListener("click",()=>{
-    translate = translate - 100;
-    bannerImg.style.transform=`translate(${translate}vw)`;
+let count = 0;
+rightSlide.addEventListener("click",function(){
+    if(count>=3)
+        count=0;
+    else
+        count++;
+    moveslide();
 })
+leftSlide.addEventListener("click",function(){
+    if(count==0)
+        count=3;
+    else
+        count--;
+    moveslide();
+})
+setInterval(()=>{
+    if(count>=3)
+        count=0;
+    else
+        count++;
+    moveslide();
+},3500);
+function moveslide(){
+    bannerImages.forEach(function(slide){
+        slide.style.transform = `translateX(-${count*100}%)`;
+    })
+}
